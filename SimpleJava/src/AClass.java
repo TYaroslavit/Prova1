b3e435960496c9b287c9cdfb8b97d296943badb7
TYaroslavit <haterzloy@gmail.com>
2022-03-20 00:10:20 +0100

    AClass example

---
+++ SimpleJava/src/AClass.java
@@ -0,0 +1,17 @@
+
+public class AClass {
+	String name;
+	String type;
+	Integer number;
+	
+	public AClass() {}
+	
+	public void setData(String name,String type,Integer number) {
+		this.name = name;
+		this.type = type;
+		this.number = number;
+	}
+	public String getData() {
+		return(this.name);
+	}
+}
